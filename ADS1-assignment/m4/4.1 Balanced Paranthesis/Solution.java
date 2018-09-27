530a7a7923b305cb2d9bbcf9fcf5115b021dab56
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-27 23:09:41 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -3,99 +3,115 @@ import java.util.Scanner;
  * Class for linked liststack.
  */
 class LinkedListstack {
-	private Node first = null;
-	private class Node {
-		char item;
-		Node nextAddress;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public char top() {
-		return first.item;
-	}
-	/**
-	 * Determines if empty.
-	 *
-	 * @return     True if empty, False otherwise.
-	 */
+    /**
+     * { var_description }
+     */
+    private Node first = null;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * { var_description }
+         */
+        char item;
+        /**
+         * { var_description }
+         */
+        Node nextAddress;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char top() {
+        return first.item;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
 
 
-	public boolean isEmpty() {
-		return first == null;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      item  The item
-	 */
-	public void push(char item) {
-		Node oldfirst = first;
-		first = new Node();
-		first.item = item;
-		first.nextAddress = oldfirst;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public char pop() {
-		char item = first.item;
-		first = first.nextAddress;
-		return item;
-	}
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      item  The item
+     */
+    public void push(char item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.nextAddress = oldfirst;
+    }
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char pop() {
+        char item = first.item;
+        first = first.nextAddress;
+        return item;
+    }
 }
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < size; i++) {
-			String line = sc.next();
-			if (Paranthesis(line)) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < size; i++) {
+            String line = sc.next();
+            if (Paranthesis(line)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
 
-		}
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static boolean Paranthesis(String s) {
-		LinkedListstack object = new LinkedListstack();
-		int length = s.length();
-		for (int i = 0; i < length; i++) {
-			char ch = s.charAt(i);
-			if (ch == '(' || ch == '{' || ch == '[') {
-				object.push(ch);
-			} else {
-				if (object.isEmpty()) return false;
-				if (ch == ')' && object.top() == '(') {
-					object.pop();
-				} else if (ch == '}' && object.top() == '{') {
-					object.pop();
-				} else if (ch == ']' && object.top() == '[') {
-					object.pop();
-				} else return false;
-			}
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static boolean Paranthesis(String s) {
+        LinkedListstack object = new LinkedListstack();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                object.push(ch);
+            } else {
+                if (object.isEmpty()){
+                   return false; 
+                } 
+                if (ch == ')' && object.top() == '(') {
+                    object.pop();
+                } else if (ch == '}' && object.top() == '{') {
+                    object.pop();
+                } else if (ch == ']' && object.top() == '[') {
+                    object.pop();
+                } else {
+                   return false; 
+                } 
+            }
 
-		}
-		return object.isEmpty();
-	}
+        }
+        return object.isEmpty();
+    }
 }
