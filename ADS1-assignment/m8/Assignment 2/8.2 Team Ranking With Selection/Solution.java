ce37eca36a4834a4b6ff965ab3ff321259732fc9
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-10-02 23:10:33 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

---
+++ ADS1-assignment/m8/Assignment 2/8.2 Team Ranking With Selection/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**.
+ *Class for Solution.
+ */
+public final class Solution {
+    /**.
+     * Magic Number.
+     */
+    private static final int TEN = 10;
+    /**.
+     * Magic Number.
+     */
+    private static final int THREE = 3;
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+
+    /**.
+     *Main function to handle test cases and deliver required output.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Team[] cricketteams = new Team[TEN];
+        int length = 0;
+        Scanner sc = new Scanner(System.in);
+        Table t = new Table();
+        while (sc.hasNext()) {
+            String[] line = sc.nextLine().split(",");
+            cricketteams[length++] = new Team(line[0], Integer.parseInt(line[1]),
+                     Integer.parseInt(line[2]), Integer.parseInt(line[THREE]));
+        }
+        System.out.println(t.leaderboard(cricketteams, length));
+    }
+}
