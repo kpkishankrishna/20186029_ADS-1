4c0a640ec25d9ac4390629a3a4f26cb14999ce9d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-10-06 12:32:56 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- ADS1-assignment/m12/Assignment-1/Solution.java
+++ ADS1-assignment/m12/Assignment-1/Solution.java
@@ -13,25 +13,22 @@ public class Solution {
         int sccatogory = Integer.parseInt(sc.nextLine());
         int stcatagory = Integer.parseInt(sc.nextLine());
         Student[] qualifiedarray = new Student[qualified];
+        Student[] reserved = new Student[qualified];
         for (int i = 0; i < qualified; i++) {
             String[] studentinfo = sc.nextLine().split(",");
             qualifiedarray[i] = new Student(studentinfo[0], studentinfo[1], Integer.parseInt(studentinfo[2]),
              Integer.parseInt(studentinfo[3]), Integer.parseInt(studentinfo[4]), Integer.parseInt(studentinfo[5]), 
              studentinfo[6]);
-        }
-        Merge.sort(qualifiedarray);
-        for (int i = 0; i < qualified; i++) {
-            System.out.println(qualifiedarray[i].studentname + ","+ qualifiedarray[i].totalmarks + "," + qualifiedarray[i].reservation);
-        }
-        System.out.println("\n");
-        Student[] reserved = new Student[qualified];
-        for (int i = 0; i < qualified; i++) {
-            String[] studentinfo = sc.nextLine().split(",");
             if (studentinfo[6].compareTo("Open") != 0)
                 reserved[i] = new Student(studentinfo[0], studentinfo[1], Integer.parseInt(studentinfo[2]),
                  Integer.parseInt(studentinfo[3]), Integer.parseInt(studentinfo[4]), Integer.parseInt(studentinfo[5]), 
                  studentinfo[6]);
         }
+        Merge.sort(qualifiedarray);
+        for (int i = 0; i < qualified; i++) {
+            System.out.println(qualifiedarray[i].studentname + ","+ qualifiedarray[i].totalmarks + "," + qualifiedarray[i].reservation);
+        }
+        System.out.println("\n");            
         Merge.sort(qualifiedarray);
         for (int i = 0; i < qualified; i++) {
             System.out.println(reserved[i].studentname + ","+ reserved[i].totalmarks + "," + reserved[i].reservation);
