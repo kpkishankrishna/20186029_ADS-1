27c855bc01993e40cb8003037d9bd07f5421ee6c
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-10-13 12:53:03 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

--- ADS1-assignment/m18/Week-3_Assignment-1/Solution.java
+++ ADS1-assignment/m18/Week-3_Assignment-1/Solution.java
@@ -127,6 +127,23 @@ public class Solution {
 			System.out.println(minpq5.delMin());
 		}
 		System.out.println();
+		MaxPQ<Stock> maxpq6 = new MaxPQ<Stock>();
+		MinPQ<Stock> minpq6 = new MinPQ<Stock>(); 
+		for (int i = 0; i < numberofcompanies; i++) {
+			String[] tokens = sc.nextLine().split(",");
+			Stock object = new Stock(tokens[0], Double.parseDouble(tokens[1]));
+			maxpq6.insert(object);
+			minpq6.insert(object);
+			count++;
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println(maxpq6.delMax());			
+
+		}
+		System.out.println();
+		for (int i =0; i < 5; i++) {
+			System.out.println(minpq6.delMin());
+		}
 		// MaxPQ<Stock> maxpq1 = new MaxPQ<Stock>(numberofcompanies);
 		// MinPQ<Stock> minpq1 = new MinPQ<Stock>(numberofcompanies); 
 		// for (int i = 0; i < numberofcompanies; i++) {
